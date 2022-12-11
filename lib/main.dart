1ab125ed6d092386b35ae5cb56dd297be41bc6a8
Yamada Simon <ymdsmn@gmail.com>
2022-12-11 16:31:32 +0900

    battery optimizer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'admin_mobile/admin_mobile.dart';
-import 'signin_signup/signin_signup.dart';
+// import 'signin_signup/signin_signup.dart';
+import 'battery_optimizer/page.dart';
 
 void main() {
   runApp(const MyApp());
