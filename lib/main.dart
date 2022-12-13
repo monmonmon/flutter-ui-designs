53756b178dbe77e681de9ff867393d9188fba89e
Yamada Simon <ymdsmn@gmail.com>
2022-12-13 15:09:01 +0900

    animation

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 // import 'admin_mobile/admin_mobile.dart';
 // import 'signin_signup/signin_signup.dart';
 // import 'battery_optimizer/page.dart';
-import 'flight_booking/page.dart';
+// import 'flight_booking/page.dart';
+import 'animation/page.dart';
 
 void main() {
   runApp(const MyApp());
